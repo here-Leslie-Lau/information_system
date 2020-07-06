582c52838f6b2b4b53f5513c652af43b11c2f5c3
here-Leslie-Lau <804738464@qq.com>
2020-07-06 13:31:36 +0800

    对科目模块进行缓存管理

--- src/main/java/leslie/information_system/service/impl/SubjectServiceImpl.java
+++ src/main/java/leslie/information_system/service/impl/SubjectServiceImpl.java
@@ -5,6 +5,8 @@ import leslie.information_system.mapper.GradeMapper;
 import leslie.information_system.mapper.SubjectMapper;
 import leslie.information_system.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +21,7 @@ public class SubjectServiceImpl implements SubjectService {
     private GradeMapper gradeMapper;
 
     @Override
+    @Cacheable("subjectList")
     public List<Subject> getAllSubject() {
         List<Subject> allSubject = subjectMapper.getAllSubject();
 
@@ -26,6 +29,7 @@ public class SubjectServiceImpl implements SubjectService {
     }
 
     @Override
+    @Cacheable("subjectListByDepartment_id")
     public List<Subject> getSubjectByDepartment_id(Integer department_id) {
         List<Subject> subjectByDepartment_id = subjectMapper.getSubjectByDepartment_id(department_id);
 
@@ -40,11 +44,13 @@ public class SubjectServiceImpl implements SubjectService {
     }
 
     @Override
+    @CacheEvict({"subjectListByDepartment_id","subjectList"})
     public void addSubject(Subject subject) {
         subjectMapper.addSubject(subject);
     }
 
     @Override
+    @CacheEvict({"subjectListByDepartment_id","subjectList"})
     public void deleteSubject(Integer subject_id) {
         gradeMapper.deleteGradeBySubject_id(subject_id);
 
@@ -52,6 +58,7 @@ public class SubjectServiceImpl implements SubjectService {
     }
 
     @Override
+    @CacheEvict({"subjectListByDepartment_id","subjectList"})
     public void updateSubject(Subject subject) {
         subjectMapper.updateSubject(subject);
     }
